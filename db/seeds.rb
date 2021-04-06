ada2dd7a9e3f4668f1df95a5efd02be9b3f69ca0
Erik Huang <ekhu94@gmail.com>
2021-04-05 20:08:34 -0700

    seeds for zelda subreddit added

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,24 +29,24 @@ def loadNintendoSwitchPosts
     }
 end
 
-# def loadZeldaPosts
-#     file = open('./db/db_zelda.json')
-#     content = file.read
-#     json = JSON.parse(content)
+def loadZeldaPosts
+    file = open('./db/db_zelda.json')
+    content = file.read
+    json = JSON.parse(content)
 
-#     json['data'].each{ |i|
-#         user_id = rand(User.first.id..User.last.id)
-#         forum_id = Forum.find_by(name: 'Zelda')
-#         Post.create(
-#             title: i['data']['title'],
-#             content: '',
-#             img_url: i['data']['url_overridden_by_dest'],
-#             user_id: user_id,
-#             forum_id: forum_id
-#         )
-#     }
-# end
+    json.each{ |i|
+        user_id = rand(User.first.id..User.last.id)
+        forum = Forum.find_by(name: 'Zelda')
+        Post.create(
+            title: i['data']['children'][0]['data']['title'],
+            content: nil,
+            img_url: i['data']['children'][0]['data']['url_overridden_by_dest'],
+            user: User.find(user_id),
+            forum: forum
+        )
+    }
+end
 
 loadForums
 loadNintendoSwitchPosts
-# loadZeldaPosts
+loadZeldaPosts
