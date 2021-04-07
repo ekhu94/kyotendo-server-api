14c1337c0226241a1ccb8121893311fb4b5a8083
Erik Huang <ekhu94@gmail.com>
2021-04-07 00:35:51 -0700

    all post seeds for Zelda done

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ def loadNintendoSwitchPosts
     disc_content = disc_file.read
     disc_json = JSON.parse(disc_content)
 
-    disc_json.each{ |i|
+    disc_json.each do |i|
         user_id = rand(User.first.id..User.last.id)
         forum = Forum.find_by(name: 'Nintendo Switch')
         Post.create(
@@ -27,16 +27,67 @@ def loadNintendoSwitchPosts
             user: User.find(user_id),
             forum: forum
         )
-    }
+    end
+
+    #* Image Posts
+    img_file = open('./db/db_nintendo_switch_img.json')
+    img_content = img_file.read
+    img_json = JSON.parse(img_content)
+
+    img_json.each do |i|
+        user_id = rand(User.first.id..User.last.id)
+        forum = Forum.find_by(name: 'Nintendo Switch')
+        Post.create(
+            title: i['data']['children'][0]['data']['title'],
+            content_url: i['data']['children'][0]['data']['url_overridden_by_dest'],
+            post_type: 'image',
+            user: User.find(user_id),
+            forum: forum
+        )
+    end
+
+    #* Video Posts
+    vid_file = open('./db/db_nintendo_switch_vid.json')
+    vid_content = vid_file.read
+    vid_json = JSON.parse(vid_content)
+
+    vid_json.each do |i|
+        user_id = rand(User.first.id..User.last.id)
+        forum = Forum.find_by(name: 'Nintendo Switch')
+        Post.create(
+            title: i['data']['children'][0]['data']['title'],
+            content_url: i['data']['children'][0]['data']['url_overridden_by_dest'],
+            post_type: 'video',
+            user: User.find(user_id),
+            forum: forum
+        )
+    end
 end
 
 def loadZeldaPosts
+    #* Discussion Posts
+    disc_file = open('./db/db_zelda_disc.json')
+    disc_content = disc_file.read
+    disc_json = JSON.parse(disc_content)
+
+    disc_json.each do |i|
+        user_id = rand(User.first.id..User.last.id)
+        forum = Forum.find_by(name: 'Zelda')
+        Post.create(
+            title: i['data']['children'][0]['data']['title'],
+            content_text: i['data']['children'][0]['data']['selftext'],
+            post_type: 'discussion',
+            user: User.find(user_id),
+            forum: forum
+        )
+    end
+
     #* Image Posts
     img_file = open('./db/db_zelda_img.json')
     img_content = img_file.read
     img_json = JSON.parse(img_content)
 
-    img_json.each{ |i|
+    img_json.each do |i|
         user_id = rand(User.first.id..User.last.id)
         forum = Forum.find_by(name: 'Zelda')
         Post.create(
@@ -46,7 +97,24 @@ def loadZeldaPosts
             user: User.find(user_id),
             forum: forum
         )
-    }
+    end
+
+    #* Video Posts
+    vid_file = open('./db/db_zelda_vid.json')
+    vid_content = vid_file.read
+    vid_json = JSON.parse(vid_content)
+
+    vid_json.each do |i|
+        user_id = rand(User.first.id..User.last.id)
+        forum = Forum.find_by(name: 'Zelda')
+        Post.create(
+            title: i['data']['children'][0]['data']['title'],
+            content_url: i['data']['children'][0]['data']['url_overridden_by_dest'],
+            post_type: 'video',
+            user: User.find(user_id),
+            forum: forum
+        )
+    end
 end
 
 def loadMarioKartPosts
