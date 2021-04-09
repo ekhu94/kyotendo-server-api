0a592c4eb27ce5f03f4cec352d1f816872b0e4df
Erik Huang <ekhu94@gmail.com>
2021-04-08 21:22:59 -0700

    add show route to User

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::UsersController < ApplicationController
-    skip_before_action :authorized, only: [:create, :index, :profile]
+    skip_before_action :authorized, only: [:create, :index, :show, :profile]
 
     def index
         users = User.all
         render json: users
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        render json: user
     end
 
     def profile
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       root 'welcome#index'
-      resources :users, only: [:create, :index]
+      resources :users, only: [:create, :index, :show]
       resources :forums
       resources :posts
       resources :videos
