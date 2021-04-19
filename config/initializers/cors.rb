3fa2002bf0117dc8e310b0f76fd6c7960e3c270d
Erik Huang <ekhu94@gmail.com>
2021-04-19 09:17:12 -0700

    retry

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.application.credentials[Rails.env.to_sym][:allowed_origins]
+    origins 'localhost:3000', 'kyotendo.herokuapp.com'
 
     resource '*',
       headers: :any,
