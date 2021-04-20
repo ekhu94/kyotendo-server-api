41e6a213b21f525ea7af7e34a8a510d7e18c3e65
Erik Huang <ekhu94@gmail.com>
2021-04-20 14:15:35 -0700

    adds created_at to post serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content_text, :content_url, :post_type, :upvotes, :user_id, :forum_id, :forum, :comments
+  attributes :id, :title, :created_at, :content_text, :content_url, :post_type, :upvotes, :user_id, :forum_id, :forum, :comments
   belongs_to :forum
   belongs_to :user
   has_many :comments
