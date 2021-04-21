00666d38092b9d7a8804ce520b989926ecab3a60
Erik Huang <ekhu94@gmail.com>
2021-04-20 19:22:49 -0700

    try change cors to new netlify site

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'kyotendo.herokuapp.com'
+    origins 'kyotendo.netlify.app'
 
     resource '*',
       headers: :any,
